c6dde690fdec835fa0b888ced8556a1f06a456a2
Danielle Santos <danielle.vtds@gmail.com>
2024-07-12 12:12:09 -0300

    iniciando backend em node

---
+++ src/lib/email.ts
@@ -0,0 +1,15 @@
+import nodemailer from "nodemailer";
+
+export async function getEmailClient() {
+    const account = await nodemailer.createTestAccount(); //cria e-mail ficticio
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: account.user,
+            pass: account.pass
+        }
+    });
+    return transporter;
+}
---
+++ src/routes/createTrip.ts
@@ -0,0 +1,112 @@
+import nodemailer from "nodemailer";
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat"
+import 'dayjs/locale/pt-br'
+import { prisma } from "../lib/prisma";
+import { getEmailClient } from "../lib/email";
+
+dayjs.locale('pt-br')
+dayjs.extend(localizedFormat)
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips",
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+          owner_name: z.string().min(4),
+          owner_email: z.string().email(),
+          emails_invite: z.array(z.string().email()),
+        }),
+      },
+    },
+    async (request) => {
+      const {
+        destination,
+        starts_at,
+        ends_at,
+        owner_name,
+        owner_email,
+        emails_invite,
+      } = request.body;
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error("Horário de ínicio da viagem inválido.");
+      }
+
+      const trip = await prisma.trip.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+          participants: {
+            createMany: {
+              data: [
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+                ...emails_invite.map((email) => {
+                  return {
+                    name: "",
+                    email,
+                    is_owner: false,
+                    is_confirmed: false,
+                  };
+                }),
+              ],
+            },
+          },
+        },
+      });
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error("Horário de finalização da viagem inválido.");
+      }
+
+      const formatStartDate = dayjs(starts_at).format('LL')
+      const formatEndDate = dayjs(ends_at).format('LL')
+
+      const confirmationLink = `https://localhost:3333/trips/${trip.id}/confirm`;
+
+      const email = await getEmailClient();
+
+      const message = await email.sendMail({
+        from: {
+          name: "Equipe de Viagem",
+          address: "h6kS8@example.com",
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: `Confirme sua viagem para ${destination} em ${formatStartDate}.`,
+        html: `
+          <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+          <p>Você solicitou a criação de uma viagem para <strong>${destination}, Brasil</strong> nas datas <strong>de ${formatStartDate}</strong> até <strong>${formatEndDate}</strong></p>
+          <p></p>
+          <p>Para confirmar sua viagem, clique no link abaixo:</p>
+          <p></p>
+          <p>
+            <a href="${confirmationLink}">Confirmar viagem</a>
+          </p>
+          <p></p>
+          <p>Caso não tenha solicitado esta viagem, ignore este e-mail.</p>
+          </div>
+          `.trim(),
+      });
+
+      console.log(nodemailer.getTestMessageUrl(message));
+
+      return { tripId: trip.id };
+    }
+  );
+}
